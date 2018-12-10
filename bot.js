07b59697acd11ea1990ec055e82002ea16228e52
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2018-12-10 19:14:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,11 @@ flixz8.on('ready', () => {
     var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf-BiTcH","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
         flixz8.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1500));
-    }, 1500);
+    }, 50000);
 });
 
 flixz8.on('ready', () => {
     setInterval(() => flixz8.channels.get('485710163966296064').send('#daily <@488388962201763840>'), 86403000);
-    }
 });
 
 flixz8.login(process.env.BOT_TOKEN);
