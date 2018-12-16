e302679744e488f4133b30087b7f15d6da8c9107
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2018-12-16 20:54:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,6 @@ flixz8.on('ready', () => {
         flixz8.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(800));
     }, 1500);
 });
-flixz8.on('ready', () => {
-    setInterval(() => flixz8.channels.get('520026772151205893').send('#daily <@488388962201763840>'), 86403000);
-});
 flixz8.login(process.env.BOT_TOKEN);
 
 const assaf = new Discord.Client();
@@ -20,16 +17,5 @@ assaf.on('ready', () => {
     setInterval(() => {
         assaf.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(800));
     }, 1500);
-});
-assaf.on('ready', () => {
-    setInterval(() => assaf.channels.get('520026772151205893).send('#daily <@488388962201763840>'), 86403000);
-});
-
-assaf.on('message', message => {
-    if(message.author.id !== '488388962201763840') return;
-    if(message.content.toLowerCase().split(' ')[0] == '1say') {
-        if(!message.content.toLowerCase().split(' ').slice(1).join(' ')) return message.channel.send('What i say?');
-        message.channel.send(message.content.toLowerCase().split(' ').slice(1).join(' '));
-    }
 });
 assaf.login(process.env.ASSAF_TOKEN);
