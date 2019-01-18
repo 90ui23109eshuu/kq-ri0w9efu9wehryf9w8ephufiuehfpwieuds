4ffcdfaa1b9f2271df46b4dfb4533d345cc7926d
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-18 16:25:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,22 @@
 const Discord = require('discord.js');
-
 const flixz8 = new Discord.Client();
-flixz8.on('ready', () => {
-    console.log('Ready.');
-    var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
-    setInterval(() => {
-        flixz8.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(800));
-    }, 5000);
+flixz8.on("ready", () => { 
+console.log('By : 3z'); flixz8.user.setPresence({ 
+       status: 'online', 
+       game: { type: 0, name: 'FlixMc',
+       details: `https://discord.gg/29KpKJy`, 
+       url: 'http://twitch.tv/flixhost', 
+       state: `Deving & Coding`, 
+      application_id: '532682436471947264', 
+
+      assets: { 
+small_image: `535061300569571329`, 
+
+small_text: 'FlixHost', 
+large_image: `535061300569571329`, large_text: `Road To 7k` } 
+
+} 
+
+}); 
 });
 flixz8.login(process.env.BOT_TOKEN);
-
-const assaf = new Discord.Client();
-assaf.on('ready', () => {
-    console.log('Ready.');
-    var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
-    setInterval(() => {
-        assaf.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(800));
-    }, 5000);
-});
-assaf.login(process.env.ASSAF_TOKEN);
-
-const khaled = new Discord.Client();
-khaled.on('ready', () => {
-    console.log('Ready.');
-    var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
-    setInterval(() => {
-        khaled.channels.find(c => c.id == '520026772151205893').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(800));
-    }, 5000);
-});
-khaled.login(process.env.KHALED_TOKEN);
