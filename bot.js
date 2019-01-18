42e5f7daad7ce28ae47a0b4287e6e6e11f2b4b17
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-18 22:13:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ flixz8.on('ready', () => {
     console.log('ready.');
     setInterval(() => {
         if(sendMessages == false) return;
-        flixz8.channels.get('535868868766466068').send('FKU BITCHS. <@!521341978659651596>');
+        flixz8.channels.get('535868868766466068').send('FKU BITCHS.');
     }, 500);
 });
 
 flixz8.on('message', message => {
-if(message.author.id != '488388962201763840','515458477053706251') return;
+if(message.author.id != '488388962201763840') return;
     if(message.content == 'go') return sendMessages = true;
     if(message.content == 'stop') return sendMessages = false;
 });
