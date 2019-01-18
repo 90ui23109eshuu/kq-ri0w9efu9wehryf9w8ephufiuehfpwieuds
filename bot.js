2f8f7be8bcf799f2f8ea1bd7435008b7290fd7ea
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-18 18:39:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,21 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 flixz8.on("ready", () => { 
-console.log('By : 3z'); flixz8.user.setPresence({ 
-       status: 'online', 
-       game: { type: 0, name: 'FlixMc',
-       details: `https://discord.gg/29KpKJy`, 
-       url: 'http://twitch.tv/flixhost', 
-       state: `Deving & Coding`, 
-      application_id: '532682436471947264', 
-
-      assets: { 
-small_image: `535061300569571329`, 
-
-small_text: 'FlixHost', 
-large_image: `535061300569571329`, large_text: `Road To 7k` } 
-
-} 
-
-}); 
-});
+flixz8.on('message', dark => {
+    
+    if (dark.content === "FuiL") {
+        setInterval(function(){
+        dark.edit('**k**')    
+        dark.edit('**ks**')    
+        dark.edit('**ksm**')
+        dark.edit('**ksmk**')
+        dark.edit('**ksmk F**')
+        dark.edit('**ksmk Fu**')
+        dark.edit('**ksmk Fui*')
+        dark.edit('**ksmk FuiL**')
+        dark.edit('**ksmk FuiL :kiss:**')
+        }, 900)
+    }
+    
+})
 flixz8.login(process.env.BOT_TOKEN);
