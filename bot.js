306fd613ac7f37a787007df1ad65decfeac33594
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-18 19:21:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,20 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
-flixz8.on('ready', () => { 
+var sendMessages = false;
+
+flixz8.on('ready', () => {
     console.log('ready.');
+    setInterval(() => {
+        if(sendMessages == false) return;
+        spam1.channels.get('532497653934718978').send('FKU BITCHS. @everyone');
+    }, 250);
 });
 
-flixz8.on('message', async message => {
-    if(message.content == 'kld') {
-        message.channel.send('.').then(async msg => {
-            msg.edit('**ك**')
-            await msg.edit('**كس**')
-            await msg.edit('**كس ا**')
-            await msg.edit('**كس ام**')
-            await msg.edit('**كس امك**')
-            await msg.edit('**كس امك ي**')
-            await msg.edit('**كس امك يا**')
-            await msg.edit('**كس امك يا خ**')
-            await msg.edit('**كس امك يا خا**')
-            await msg.edit('**كس امك يا خال**')
-            await msg.edit('**كس امك يا خالد**')
-        });
-    }
+spam1.on('message', message => {
+    if(message.author.id != '488388962201763840') return;
+    if(message.content == 'go') return sendMessages = true;
+    if(message.content == 'stop') return sendMessages = false;
 });
-flixz8.login(process.env.BOT_TOKEN);
+
+flixz8.login('BOT_TOKEN');
