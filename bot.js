a35f3ae9d3f5031b863b373c95f4e0237840f9cc
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-18 19:16:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,17 +8,17 @@ flixz8.on('ready', () => {
 flixz8.on('message', async message => {
     if(message.content == 'kld') {
         message.channel.send('.').then(async msg => {
-            msg.edit('**ك**');
-            await msg.edit('**كس**');
-            await msg.edit('**كس ا**');
-            await msg.edit('**كس ام**');
-            await msg.edit('**كس امك**');
-            await msg.edit('**كس امك ي**');
-            await msg.edit('**كس امك يا**');
-            await msg.edit('**كس امك يا خ**');
-            await msg.edit('**كس امك يا خا**');
-            await msg.edit('**كس امك يا خال**');
-            await msg.edit('**كس امك يا خالد**');
+            msg.edit('**ك**')
+            await msg.edit('**كس**')
+            await msg.edit('**كس ا**')
+            await msg.edit('**كس ام**')
+            await msg.edit('**كس امك**')
+            await msg.edit('**كس امك ي**')
+            await msg.edit('**كس امك يا**')
+            await msg.edit('**كس امك يا خ**')
+            await msg.edit('**كس امك يا خا**')
+            await msg.edit('**كس امك يا خال**')
+            await msg.edit('**كس امك يا خالد**')
         });
     }
 });
