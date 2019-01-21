6b0f7c6e60d7e28393bd230b77d2e98b18658400
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-21 14:41:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
 flixz8.on('message', message => {
+    if(message.author.id != flixz8.user.id) return;
     if(message.content == '3clear') {
         message.delete();
         message.channel.messages.filter(m => m.author.id == flixz8.user.id).forEach(m => m.delete());
