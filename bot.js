73a27f4f424a059db14d16ca201476f71cab4ef4
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-26 11:25:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
-flixz8.on('message', message => {
-    if(message.author.id != flixz8.user.id) return;
-    if(message.content == '3clear') {
-        message.delete();
-        message.channel.messages.filter(m => m.author.id == flixz8.user.id).forEach(m => m.delete());
-    }
+flixz8.on('ready', () => {
+    console.log('Ready.');
+    var words = ["87987","ijdojsdosij","ShutUpNiggA","iLovey","NiggauS","NoMerrCy-Nigga","Assaf-uH","iLovepunur","i96578657865","i867567578","AbdulAzizIsKiNgG","iLoveMe"];
+    setInterval(() => {
+        flixz8.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1000));
+    }, 5000);
 });
 
-flixz8.login(process.env.BOT_TOKEN);
+flixz8.login('process.env.BOT_TOKEN');
