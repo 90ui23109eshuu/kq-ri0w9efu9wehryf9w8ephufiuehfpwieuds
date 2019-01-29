cd1c877f2722854f5648774c3a5902e40ad999f6
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-29 14:02:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
-flixz8.on('ready', () => {
-    console.log('Ready.');
-    var words = ["78689769876","Fuiohu","ShutUpNiggA","iM7668976876","NiggaS","NoMerrCy-Nigga","As68768976","inder","iWood","o09-09ider","AbdulAzizIsKiNgG","iLoveMe"];
-    setInterval(() => {
-        flixz8.channels.find(c => c.id == '527030852803100672').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1200000));
-    }, 9000000);
-});
-
 flixz8.on('message', message => {
     if(message.author.id !== flixz8.user.id) return;
     var words = ["كسمك","قحبة","خنيث","منيوك"];
