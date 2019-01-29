394df1641ba47de7de3da5b34017d15cd1b41c63
2fyh2wiy789327y7 <43373677+90ui23109eshuu@users.noreply.github.com>
2019-01-29 09:28:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ flixz8.on('ready', () => {
     console.log('Ready.');
     var words = ["78689769876","Fuiohu","ShutUpNiggA","iM7668976876","NiggaS","NoMerrCy-Nigga","As68768976","inder","iWood","o09-09ider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
-        flixz8.channels.find(c => c.id == '527030852803100672').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(2000));
+        flixz8.channels.find(c => c.id == '527030852803100672').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(60000));
     }, 5000);
 });
 
